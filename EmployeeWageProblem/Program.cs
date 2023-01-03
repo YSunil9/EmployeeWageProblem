2deb9f2c0386ed45eece14c51767f1aa8ea7657e
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-03 23:03:40 +0530

    [SUNIL]:Add Switch Case Statement

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -2,31 +2,29 @@
 {
     internal class Program
     {
+        public const int IS_FULL_TIME=1,IS_PART_TIME=2;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Employee Wage Problem\n");
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
+            Console.WriteLine("Welcome To Employee Wage Problem\n");            
             int EMP_RATE_PER_HOUR = 20;
             int EmpHrs = 0;
             int EmpWage = 0;
-
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_FULL_TIME)
+            switch(empCheck)
             {
-                Console.WriteLine("EMPLOYEE IS PRESENT FULL TIME");
-                EmpHrs = 8;
-            }
-            else if(empCheck == IS_PART_TIME)
-            {
-                Console.WriteLine("EMPLOYEE IS  PRESENT HALF TIME");
-                EmpHrs = 4;
-            }
-            else
-            {
+                case IS_FULL_TIME:
+                    Console.WriteLine("EMPLOYEE IS PRESENT FULL TIME");
+                    EmpHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    Console.WriteLine("EMPLOYEE IS  PRESENT HALF TIME");
+                    EmpHrs = 4;
+                    break;
+            default:           
                 Console.WriteLine("EMPLOYEE IS ABSENT");
                 EmpHrs=0;
+                    break;
             }
             EmpWage= EmpHrs*EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee Wage is : "+EmpWage);
