80bfcabfbe62cd8bbba5a8a19c0c8d43be35e386
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-03 22:46:22 +0530

    [SUNIL]:Add Part Time Employee & Wage

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -4,26 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Employee Wage Problem");
+            Console.WriteLine("Welcome To Employee Wage Problem\n");
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
-            int empHrs = 0;
-            int empWage = 0;
+            int EmpHrs = 0;
+            int EmpWage = 0;
 
             Random random = new Random();
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("EMPLOYEE IS PRESENT");
-                empHrs = 8;
+                Console.WriteLine("EMPLOYEE IS PRESENT FULL TIME");
+                EmpHrs = 8;
+            }
+            else if(empCheck == IS_PART_TIME)
+            {
+                Console.WriteLine("EMPLOYEE IS  PRESENT HALF TIME");
+                EmpHrs = 4;
             }
             else
             {
                 Console.WriteLine("EMPLOYEE IS ABSENT");
-                empHrs=0;
+                EmpHrs=0;
             }
-            empWage= empHrs* EMP_RATE_PER_HOUR;
-            Console.WriteLine("Employee Wage is : "+empWage);
+            EmpWage= EmpHrs*EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage is : "+EmpWage);
         }
     }
 }
