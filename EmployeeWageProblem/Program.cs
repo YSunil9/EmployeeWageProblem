b73445906067b4d9103e0b7e892c97f64fa32890
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-03 22:00:12 +0530

    Welcome To Employee Wage Problem

---
+++ EmployeeWageProblem/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace EmployeeWageProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Employee Wage Problem");
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("EMPLOYEE IS PRESENT");
+            }
+            else
+            {
+                Console.WriteLine("EMPLOYEE IS ABSENT");
+            }
+        }
+    }
+}
