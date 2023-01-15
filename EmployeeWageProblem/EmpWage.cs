b3de33f250eab05207a6057ac6c2722775e7fabd
Ysunil9 <sunil.pcs24@gmail.com>
2023-01-15 22:40:52 +0530

    [SUNIL]:Manage Wage For multiple Company

--- EmployeeWageProblem/EmpWage.cs
+++ EmployeeWageProblem/EmpWage.cs
@@ -6,29 +6,35 @@ using System.Threading.Tasks;
 
 namespace EmployeeWageProblem
 {
-    internal class EmpWage
+    internal class Builder
     {
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
-        public int MONTHLY_WORKING_DAYS;
-        public int MAX_HRS_IN_MONTH;
-        public int EMP_RATE_PER_HOUR;
-        public  string company;
-        int totalEmpWage;
-        public EmpWage(string company, int EMP_RATE_PER_HOUR, int MONTHLY_WORKING_DAYS, int MAX_HRS_IN_MONTH)
+        private int numberOfCompanies = 0;
+        private EmpWage[] companyEmpWagearray;
+        public Builder()
         {
-            this.company = company;
-            this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
-            this.MONTHLY_WORKING_DAYS = MONTHLY_WORKING_DAYS;
-            this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
+            this.companyEmpWagearray = new EmpWage[5];
         }
-        public  void ComputeEmpWage()
+        public void addtoArray(string company, int EMP_RATE_PER_HOUR, int MONTHLY_WORKING_DAYS, int MAX_HRS_IN_MONTH)
         {
-            Console.WriteLine("Welcome To Employee Wage Problem\n");
+            companyEmpWagearray[this.numberOfCompanies] = new EmpWage(company, EMP_RATE_PER_HOUR, MONTHLY_WORKING_DAYS, MAX_HRS_IN_MONTH);
+            numberOfCompanies++;
+        }
+            public void ComputeEmpWage()
+            {
+                for (int i = 0; i < numberOfCompanies; i++)
+                {
+                    companyEmpWagearray[i].setTotalEmpWage((this.ComputeEmpWage(this.companyEmpWagearray[i])));
+                    Console.WriteLine(this.companyEmpWagearray[i].companyNameAndSalary());
+                }
+            }
+        public int ComputeEmpWage(EmpWage EmployeeWage)
+        {
             int EmpHrs = 0;
             int totalEmpHrs = 0;
             int TotalWorkingDays = 0;
-            while (totalEmpHrs <= this.MAX_HRS_IN_MONTH && TotalWorkingDays < this.MONTHLY_WORKING_DAYS)
+            while (totalEmpHrs <= EmployeeWage.MAX_HRS_IN_MONTH && TotalWorkingDays < EmployeeWage.MONTHLY_WORKING_DAYS)
             {
                 TotalWorkingDays++;
                 {
@@ -53,12 +59,32 @@ namespace EmployeeWageProblem
                     Console.WriteLine("Days#:" + TotalWorkingDays + " Emp Hrs : " + EmpHrs);
                 }
             }
-            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("\ncompany name : " + company + " Total Emp Wage : " + totalEmpWage);       
-        }
-        public String ToString()
-        {
-            return "Total EmpWage For Company :" + this.company + " is:" + this.totalEmpWage;
-        }
+            return totalEmpHrs * EmployeeWage.EMP_RATE_PER_HOUR;
+        }           
+    }
+}
+public class EmpWage
+{
+
+    public int MONTHLY_WORKING_DAYS;
+    public int MAX_HRS_IN_MONTH;
+    public int EMP_RATE_PER_HOUR, totalEmpWage;
+    public string company;
+
+    public EmpWage(string company, int EMP_RATE_PER_HOUR, int MONTHLY_WORKING_DAYS, int MAX_HRS_IN_MONTH)
+    {
+        this.company = company;
+        this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
+        this.MONTHLY_WORKING_DAYS = MONTHLY_WORKING_DAYS;
+        this.MAX_HRS_IN_MONTH = MAX_HRS_IN_MONTH;
+        int totalEmpWage;
+    }
+    public void setTotalEmpWage(int totalEmpWage)
+    {
+        this.totalEmpWage = totalEmpWage;
+    }
+    public string companyNameAndSalary()
+    {
+        return "Company Name : " + this.company + "\nEmployerSalary : " + this.totalEmpWage;
     }
 }
--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -4,12 +4,22 @@
     {
         static void Main(string[] args)
         {
-            EmpWage wipro = new EmpWage("Wipro", 20, 27, 165);
-            EmpWage Reliance = new EmpWage("Reliance", 18, 20, 132);
-            wipro.ComputeEmpWage();
-            Console.WriteLine(wipro.ToString);
-            Reliance.ComputeEmpWage();
-            Console.WriteLine(Reliance.ToString);
+            Builder builder = new Builder();
+            builder.addtoArray("Wipro", 20, 27, 165);
+            builder.addtoArray("Reliance", 18, 20, 132);
+            builder.ComputeEmpWage();
+            //EmpWage wipro = new EmpWage("Wipro", 20, 27, 165);
+            //EmpWage Reliance = new EmpWage("Reliance", 18, 20, 132);
+            //wipro.ComputeEmpWage();
+            //Console.WriteLine(wipro.ToString);
+            //Reliance.ComputeEmpWage();
+            //Console.WriteLine(Reliance.ToString);
+            //EmpWageBuilderObject tata = new EmpWageBuilderObject("Tata", 14, 24, 104);
+            //EmpWageBuilderObject mahindra = new EmpWageBuilderObject("Mhaindra", 18, 25, 108);
+            //tata.ComputionEmpWage();
+            //Console.WriteLine(tata.ToString());
+            //mahindra.ComputionEmpWage();
+            //Console.WriteLine(mahindra.ToString());
         }
     }
 }
